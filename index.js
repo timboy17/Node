7b337e754e0e293a3b62ecc60b8b0475e2d1b86a
Tim <100614215+timboy17@users.noreply.github.com>
2024-02-18 23:57:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 //Put all of you files in the /public folder
 app.use(express.static(__dirname + '/public'));
+app.get(hi);
 //404
 app.use(function(req, res, next) {
   res.status(404);
